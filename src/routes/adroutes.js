29f8359c05cd9903c22eb405563aee80960c928d
Ritik Palariya <ritikpalariya2005@gmail.com>
2026-02-19 22:17:42 +0530

    Removed old files for team contribution setup

--- src/routes/adroutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { generateAd } = require("../controllers/adcontroller");
-
-router.post("/generate-ad", generateAd);
-
-module.exports = router;
