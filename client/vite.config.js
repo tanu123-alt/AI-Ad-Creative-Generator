c29bc77164a73b8d8b62215d1b42d4e7183d8cb8
sonalidevloper <sjena6401@gmail.com>
2026-03-16 16:14:34 +0530

    vite config added

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,14 +7,14 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://ai-ad-creative-generator.onrender.com",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
       "/generated": {
-        target: "http://localhost:5000",
+        target: "https://ai-ad-creative-generator.onrender.com",
         changeOrigin: true,
-        secure: false,
+        secure: true,
       },
     },
   },
